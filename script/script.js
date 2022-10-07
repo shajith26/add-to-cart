22e49f996a4bfb6151b3f6ecb07baf437c5af4ef
shajith26 <mohamedshajith321@gmail.com>
2022-10-07 16:32:08 +0530

    task

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 let btn1=document.getElementById('shop'),
-    btn2=document.getElementById('add');
+    btn2=document.getElementById('add'),
+    increaseEle=document.getElementById('increase'),
+    decreaseEle=document.getElementById('decrease'),
+    deleteEle=document.getElementById('delete');
+
 let products=[
     {productName:"tv",price:10000,id:1,qty:0},
     {productName:"wm",price:20000,id:2,qty:0},
@@ -11,14 +15,11 @@ let wishlist=[];
 function reset(){
     wishlist=[];
 }
-
-
+var x;
 function some(){
 
     
-    let x = parseInt(prompt());
-
-
+     x = parseInt(prompt());
     
 
     let result=products.find(function(order){
@@ -27,16 +28,19 @@ function some(){
     wishlist.push(result);
     console.log(wishlist);
 
+
+}btn2.addEventListener('click',some);
+
 function add(){
     let y = products.find(function(s){
-        if(s.id==x){
+        if(s.id===x){
             return s.qty+=1;
         }else{
-            return s.id == x ;
+            return s.id === x ;
         }
     
     });
-    console.log(y)
+    // console.log(y)
     reset();
     wishlist.push(y);
     console.log(wishlist);
@@ -44,4 +48,62 @@ function add(){
 
 }btn1.addEventListener('click',add);
 
-}btn2.addEventListener('click',some);
+
+
+// {
+//     function greeting(y) {
+//         alert(`Hello, ${y}`);
+//       }
+      
+//       function processUserInput() {
+//         const name = prompt("Please enter your name.");
+//         greeting(name);
+//       }
+      
+//       processUserInput();
+
+
+
+//       let a="Mohamed";
+//       let b="Shajith";
+
+//       let x= `i'm ${a} ${b}`;
+
+//       console.log(x)
+
+//       //promises
+//       let x=5;
+//       let p=new Promise(function(resolve,reject){
+
+//         if(x>3){
+//             resolve("success");
+//         }else{
+//             reject("failed");
+//         }
+//       })
+//       p.then(function(message){
+//         console.log("then "+ message);
+//       }).catch(function(message){
+//         console.log("catch "+message)
+//       })
+
+
+
+//       //which word has more letters
+
+//       let a='bike';
+//       let b='car';
+
+//       let p=new Promise(function(resolve,reject){
+//         if(a.length>b.length){
+//             resolve("bike");
+//         }else{
+//             reject("car");
+//         }
+//       })
+//       p.then(function(answer){
+//         console.log('B '+answer);
+//       }).catch(function(ans){
+//         console.log('C '+ans);
+//       })
+// }
